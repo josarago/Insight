2738c0ebb28d122f23f02053ce9c3ee115e9a39b
Jonathan Saragosti <saragostij+github@gmail.com>
2018-09-25 17:01:26 -0400

    first dash draft
    - displays top 10 lines of table
    - some styling
    - text input

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+
+df = pd.read_csv("winemag-data-130k-v2.csv")
+
+
+def generate_table(dataframe, max_rows=10,columns = ['title','description','variety','region_1','country','price','points']):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 
 app = dash.Dash()
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+app.layout = html.Div(children=[
+    html.H4(children='Find some cool wine'),
+    dcc.Input(id='my-id', value='initial value', type='text',style={'width': '100%','display': 'inline-block'}),
+    html.Div(id='my-div'),
+    generate_table(df),
 
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
 ])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value.lower())
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
