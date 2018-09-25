f7d2451bf2e46378dd9adbfb2cd7be15c8883f11
Jonathan Saragosti <saragostij+github@gmail.com>
2018-09-25 19:50:39 -0400

    outlined the find_wines function

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = dash.Dash()
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div(children=[
-    html.H4(children='Find some cool wine'),
+    html.H4(children='Cooler Wines'),
     dcc.Input(id='input-text', value='initial value', type='text',style={'width': '100%','display': 'inline-block'}),
     html.Div(id='results'),
     # generate_table(df),
@@ -36,7 +36,36 @@ app.layout = html.Div(children=[
     Output(component_id='results', component_property='children'),
     [Input(component_id='input-text', component_property='value')]
 )
-def do_something_cool(input_value):
+def find_wines(input_value):
+    """
+        behavior:
+            user can enter:
+                - variety (will be shown wine of that variety)
+                - region (will be shown wine of that variety)
+                - any keywords
+
+            always start by presenting results that have exact matches,
+            then present results from Doc2vec
+    """
+    # tokenize the input_value
+
+    # for each token
+        # check if varieties or regions are present
+
+    # if there is more than one variety or one region
+        #throw some error
+
+    # if there is only one variety
+        # return message "so yeah we have n wines of this variety but that's boring. try .. instead"
+    # if there is only one region
+        # return message "so yeah we have n wines of this region but that's boring. try .. instead"
+
+    # if user enter keywords
+        # try to find exact results
+            # if some exact matches are found display them
+
+        # then display ML suggested options
+
     try:
         idx = int(input_value)
         return html.Table(
@@ -45,8 +74,9 @@ def do_something_cool(input_value):
             # Body
             [html.Tr([html.Td(df.iloc[idx][col]) for col in df_columns])]
         )
-    except Exception as e: print(e)
-
+    except Exception as e:
+        print(e)
+        return "enter a integer"
 
 
 if __name__ == '__main__':
